13171606f753c2d0df3d65922db9bffab780c550
BlinkersFr31 <36617780+BlinkersFr31@users.noreply.github.com>
2024-03-06 11:38:55 +0100

    add gestion mails

--- SpamShield/mailFiltering/static/js/liste.js
+++ SpamShield/mailFiltering/static/js/liste.js
@@ -9,3 +9,19 @@ function checkAllMsgs() {
 function showMessageForServeur() {
 	window.open("/mailFiltering/" + $( "select[name='serveur']" ).val(), "_self");
 }
+
+function actionForServeur() {
+	var actionVal = $("#action")[0].value;
+	var messagesChckVal = $( "input[name='messagesChck']:checked");
+	var serveurId = $( "select[name='serveur']" ).val();
+	var urlAjx = '/mailFiltering/gestionMails/' + serveurId;
+	var jsonVal = JSON.stringify({ action: actionVal, mailsUID: messagesChckVal, domains: "" });
+	$.ajax({
+		type: 'POST',
+		url: urlAjx,
+		data: jsonVal,
+		success: function(data) { alert('data: ' + data); },
+		contentType: "application/json",
+		dataType: 'json'
+	});
+}
