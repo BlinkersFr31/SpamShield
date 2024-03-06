45408e8e75f214ca052f8e91682539b7eecdc367
BlinkersFr31 <36617780+BlinkersFr31@users.noreply.github.com>
2024-03-06 10:10:12 +0100

    Add gestionMails

--- SpamShield/mailFiltering/urls.py
+++ SpamShield/mailFiltering/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:serveur_id>", views.listemails, name="listemails"),
+    path("gestionMails/<int:serveur_id>", views.gestionMails, name="gestionMails"),
     path("serveurs", views.serveurs, name="serveurs"),
     path("serveur/<int:serveur_id>", views.serveur, name="serveur"),
     path("<int:serveur_id>/saveServeur", views.saveServeur, name="saveServeur"),
